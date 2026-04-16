1d504c772b13ace84bae4d9195b6dfb5084ab4d2
Prince <princekr1603@gmail.com>
2026-04-16 22:48:54 +0530

    update

---
+++ Array/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    public static int Binarysearch(int arr[],int target){
+        int low=0, high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==arr[mid]){
+                return mid;
+            }
+            else if(target>arr[mid]){
+                low=mid+1;
+            }
+            else if(target<arr[mid]){
+                high=mid-1;
+            }
+        }
+        return-1;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,7,8,11,22,34,65,78,88};
+        int target=78;
+        int result= Binarysearch(arr,target);
+        if(result==-1){
+            System.out.println("element not present");
+        }
+        else{
+            System.out.println("element present at index "+result);
+        }
+    }
+}
