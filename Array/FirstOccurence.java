8434e4a80c358e1eed68987f2ebdf774010b6204
Prince <princekr1603@gmail.com>
2026-04-16 22:56:08 +0530

    update

---
+++ Array/FirstOccurence.java
@@ -0,0 +1,33 @@
+public class FirstOccurence {
+    public static int FirstFound(int arr[],int target){
+        int low=0, high=arr.length-1,idx=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(target>arr[mid]){
+                low=mid+1;
+            }
+            else if(target<arr[mid]){
+                high=mid-1;
+            }
+            else if(target==arr[mid]){
+                idx=mid;
+                high=mid-1;
+            }
+        }
+        return idx;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,2,3,7,8,11,22,34,65,78,78,78,88};
+        int target=78;
+        int result= FirstFound(arr,target);
+        if(result==-1){
+            System.out.println("element not present");
+        }
+        else{
+            System.out.println("element present at index "+result);
+        }
+    }
+}
+
+
