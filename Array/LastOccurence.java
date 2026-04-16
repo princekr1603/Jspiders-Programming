a95a3afe9cf1694ae54b500eb41f6ea9b5b4e1f4
Prince <princekr1603@gmail.com>
2026-04-16 23:01:44 +0530

    update

---
+++ Array/LastOccurence.java
@@ -0,0 +1,31 @@
+public class LastOccurence {
+    public static int FirstFound(int arr[],int target){
+        int low=0, high=arr.length-1,idx=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(target>arr[mid]){
+                low=mid+1;
+            }
+            else if(target<arr[mid]){
+                high=mid-1;
+            }
+            else if(target==arr[mid]){
+                idx=mid;
+                low=mid+1;
+            }
+        }
+        return idx;
+    }
+        public static void main(String[] args) {
+        int[] arr={1,2,2,3,7,8,11,22,34,65,78,78,78,88};
+        int target=78;
+        int result= FirstFound(arr,target);
+        if(result==-1){
+            System.out.println("Element not present");
+        }
+        else{
+            System.out.println("Element present at index "+result);
+        }
+    }
+}
